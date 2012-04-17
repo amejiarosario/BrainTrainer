2d8d2cc2089772b2ef875888cf3e0662a1ff8103
Adrian Mejia <axm7988@rit.edu>
2012-04-17 01:05:16 -0400

    still fighting with nested forms

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -2,5 +2,6 @@ class Exercise < ActiveRecord::Base
   has_many :operations
   attr_accessible :long_description, :no, :short_description
   validates :long_description, :no, :short_description, presence: true
-  
+  accepts_nested_attributes_for :operations, allow_destroy: :true
+
 end
