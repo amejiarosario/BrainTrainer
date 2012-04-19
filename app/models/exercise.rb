17fa33cffb97b61c18d911ca68368cda3d94855b
Adrian Mejia <axm7988@rit.edu>
2012-04-19 00:19:52 -0400

    each exercise display its operations properly some formating and textbox for answers are missing though

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,9 +1,8 @@
 class Exercise < ActiveRecord::Base
   has_many :operations, dependent: :destroy
-  accepts_nested_attributes_for :operations, allow_destroy: :true
+  accepts_nested_attributes_for :operations, allow_destroy: :true, reject_if: lambda {|a| a[:numbers].blank? }
   
   attr_accessible :long_description, :no, :short_description, :operations_attributes
   validates :long_description, :no, :short_description, presence: true
   
-
 end
