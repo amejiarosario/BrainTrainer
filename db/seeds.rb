5a466dc45399e8f8a97f584d728ce8a82cc845f1
Adrian Mejia <axm7988@rit.edu>
2012-04-21 23:46:31 -0400

    seed updated until exercise no. 14

--- db/seeds.rb
+++ db/seeds.rb
@@ -257,42 +257,19 @@ e.operations << o
 e = Exercise.create!(no: 3, 
       short_description: "Add pairs that sum 10", 
       long_description: "Add pairs of succesive number that sum 10. NOTE: add them from the bottom up.")
-o = Operation.create!(numbers:"7,8,4,6,5,3,5,5,1,8,2,5", operator: "+", align:"vertical")
-e.operations << o
-o = Operation.create!(numbers:"6,4,5,2,4,5,4,1,2,8,7,3", operator: "+", align:"vertical")
-e.operations << o
-o = Operation.create!(numbers:"5,2,5,4,6,6,7,3,4,8,2,4", operator: "+", align:"vertical")
-e.operations << o
-o = Operation.create!(numbers:"9,7,6,4,8,8,9,1,1,7,5,5", operator: "+", align:"vertical")
-e.operations << o
-o = Operation.create!(numbers:"6,7,9,1,3,4,6,3,8,5,2,8", operator: "+", align:"vertical")
-e.operations << o
-o = Operation.create!(numbers:"3,1,6,4,4,1,8,2,9,6,4,7", operator: "+", align:"vertical")
-e.operations << o
-o = Operation.create!(numbers:"4,7,3,8,3,2,2,8,1,9,1,9", operator: "+", align:"vertical")
-e.operations << o
-o = Operation.create!(numbers:"8,2,9,1,5,3,8,5,5,2,6,5", operator: "+", align:"vertical")
-e.operations << o
-o = Operation.create!(numbers:"4,4,3,2,4,6,1,6,4,9,3,7", operator: "+", align:"vertical")
-e.operations << o
-o = Operation.create!(numbers:"6,5,7,3,4,2,8,9,1,3,2,1", operator: "+", align:"vertical")
-e.operations << o
-o = Operation.create!(numbers:"9,8,8,2,7,1,9,6,5,5,5,4", operator: "+", align:"vertical")
-e.operations << o
-o = Operation.create!(numbers:"3,7,6,6,1,2,7,6,4,5,5,6", operator: "+", align:"vertical")
-e.operations << o
-o = Operation.create!(numbers:"7,4,6,3,2,6,4,1,8,3,7,9", operator: "+", align:"vertical")
-e.operations << o
-o = Operation.create!(numbers:"3,7,8,2,8,5,5,8,2,7,1,9", operator: "+", align:"vertical")
-e.operations << o
-o = Operation.create!(numbers:"9,1,6,3,7,5,4,6,4,3,2,9", operator: "+", align:"vertical")
-e.operations << o
-o = Operation.create!(numbers:"1,8,7,5,5,6,7,3,5,4,4,6", operator: "+", align:"vertical")
-e.operations << o
-o = Operation.create!(numbers:"3,6,4,2,8,5,1,4,1,9,3,7", operator: "+", align:"vertical")
-e.operations << o
-o = Operation.create!(numbers:"6,9,1,7,7,3,2,1,5,2,9,1", operator: "+", align:"vertical")
-e.operations << o
+no3 = ["7,8,4,6,5,3,5,5,1,8,2,5","6,4,5,2,4,5,4,1,2,8,7,3","5,2,5,4,6,6,7,3,4,8,2,4","9,7,6,4,8,8,9,1,1,7,5,5",
+  "6,7,9,1,3,4,6,3,8,5,2,8","3,1,6,4,4,1,8,2,9,6,4,7","4,7,3,8,3,2,2,8,1,9,1,9","8,2,9,1,5,3,8,5,5,2,6,5",
+  "4,4,3,2,4,6,1,6,4,9,3,7","6,5,7,3,4,2,8,9,1,3,2,1","9,8,8,2,7,1,9,6,5,5,5,4","3,7,6,6,1,2,7,6,4,5,5,6",
+  "7,4,6,3,2,6,4,1,8,3,7,9","3,7,8,2,8,5,5,8,2,7,1,9","9,1,6,3,7,5,4,6,4,3,2,9","1,8,7,5,5,6,7,3,5,4,4,6",
+  "3,6,4,2,8,5,1,4,1,9,3,7", "6,9,1,7,7,3,2,1,5,2,9,1"]
+def operations(e,list,operator="+",align="vertical")
+  list.each do |n|
+    o = Operation.create!(numbers: n, operator: operator, align: align)
+    e.operations << o    
+  end
+end
+
+operations(e,no3)
 
 e = Exercise.create!(no: 4, 
       short_description: "Mental Addition add 12", 
@@ -986,16 +963,6 @@ e.operations << o
 o = Operation.create!(numbers:"73", operator: "+#{sum}", align:"horizontal")
 e.operations << o
 
-sum = 14
-e = Exercise.create!(no: 7, 
-      short_description: "Mental Addition add #{sum}", 
-      long_description: "Add #{sum} to each of the numbers.\nFirst add the tens of one number to the whole other and then the units. \nFor example:\n34+#{sum} say 34,#{34+(sum-sum%10)},#{34+sum}")
-list = [1,57,13,69,25,81,37,93,56,12,68,24,80,8,64,20,76,32,88,44,7,63,19,75,31,87,15,71,27,83,39,95,51,14,70,26,82,38,94,22,78,34,90,46,2,58,21,77,33,89,45,29,85,41,97,53,9,65,28,84,40,96,52,36,92,48,4,60,15,72,35,91,47,3,59,43,99,55,11,67,23,79,42,98,54,10,66,50,6,62,18,74,30,86,49,5,61,17,73]
-list.each do |n|
-  o = Operation.create!(numbers: n.to_s, operator: "+#{sum}", align:"horizontal")
-  e.operations << o
-end
-
 sum = 15
 e = Exercise.create!(no: 7, 
       short_description: "Mental Addition add #{sum}", 
@@ -1005,28 +972,86 @@ list.each do |n|
   o = Operation.create!(numbers: n.to_s, operator: "+#{sum}", align:"horizontal")
   e.operations << o
 end
+ex = 7
 
-sum = 16
-e = Exercise.create!(no: 7, 
+
+###########################################################
+ex+=1
+e = Exercise.create!(no: ex, 
+      short_description: "Group of numbers adding 10 or less.", 
+      long_description: "In this exercise, you do groups of successive numbers that sum 10 or less. For example: 7,17,23,32; the second: 8,18,23,30; third: 9,19,29")
+no1.each do |n|
+  o = Operation.create!(numbers: n.to_s, operator: "+", align:"vertical")
+  e.operations << o
+end
+###########################################################
+
+###########################################################
+# SUM TABLE I
+###########################################################
+ex+=1
+sum +=1
+e = Exercise.create!(no: ex, 
       short_description: "Mental Addition add #{sum}", 
       long_description: "Add #{sum} to each of the numbers.\nFirst add the tens of one number to the whole other and then the units. \nFor example:\n34+#{sum} say 34,#{34+(sum-sum%10)},#{34+sum}")
-list = [1,57,13,69,25,81,37,93,56,12,68,24,80,8,64,20,76,32,88,44,7,63,19,75,31,87,15,71,27,83,39,95,51,14,70,26,82,38,94,22,78,34,90,46,2,58,21,77,33,89,45,29,85,41,97,53,9,65,28,84,40,96,52,36,92,48,4,60,15,72,35,91,47,3,59,43,99,55,11,67,23,79,42,98,54,10,66,50,6,62,18,74,30,86,49,5,61,17,73]
 list.each do |n|
   o = Operation.create!(numbers: n.to_s, operator: "+#{sum}", align:"horizontal")
   e.operations << o
 end
+###########################################################
 
-sum = 17
-e = Exercise.create!(no: 7, 
-      short_description: "Mental Addition add #{sum}", 
-      long_description: "Add #{sum} to each of the numbers.\nFirst add the tens of one number to the whole other and then the units. \nFor example:\n34+#{sum} say 34,#{34+(sum-sum%10)},#{34+sum}")
-list = [1,57,13,69,25,81,37,93,56,12,68,24,80,8,64,20,76,32,88,44,7,63,19,75,31,87,15,71,27,83,39,95,51,14,70,26,82,38,94,22,78,34,90,46,2,58,21,77,33,89,45,29,85,41,97,53,9,65,28,84,40,96,52,36,92,48,4,60,15,72,35,91,47,3,59,43,99,55,11,67,23,79,42,98,54,10,66,50,6,62,18,74,30,86,49,5,61,17,73]
-list.each do |n|
-  o = Operation.create!(numbers: n.to_s, operator: "+#{sum}", align:"horizontal")
-  e.operations << o
+###########################################################
+# SUM TABLE I
+###########################################################
+def table1(e,sum,ex,list)
+  e = Exercise.create!(no: ex, 
+        short_description: "Mental Addition add #{sum}", 
+        long_description: "Add #{sum} to each of the numbers.\nFirst add the tens of one number to the whole other and then the units. \nFor example:\n34+#{sum} say 34,#{34+(sum-sum%10)},#{34+sum}")
+  list.each do |n|
+    o = Operation.create!(numbers: n.to_s, operator: "+#{sum}", align:"horizontal")
+    e.operations << o
+  end
 end
+ex+=1
+sum +=1
+table1(e,sum,ex,list)
+###########################################################
 
+
+###########################################################
+ex+=1
+e = Exercise.create!(no: ex, 
+      short_description: "Group of numbers adding 10 or less.", 
+      long_description: "In this exercise, you do groups of successive numbers that sum 10 or less. For example: 7,17,23,32; the second: 8,18,23,30; third: 9,19,29")
+operations(e,no3)
+###########################################################
+
+#12
+ex+=1
+sum +=1
+table1(e,sum,ex,list)
 # 
-# u.exercises << e
-# u.scores.last.attempts = 1
-# u.scores.last.time = 236
+
+###########################################################
+ex+=1
+e = Exercise.create!(no: ex, 
+      short_description: "Adding single columns by pairs.", 
+      long_description: "Add from the top down: add 2 successive number by single columns. For example: 5,14,25, write 5 and carry 2; then in the 2nd column: 2,12,27,36; thus, the answer is 365.")
+no13 = ["43,62,78,81,14,87","29,75,36,69,43,16","58,33,65,98,72,45","87,62,94,27,89,74","16,91,33,56,29,32",
+  "16,99,36,71,61,41","48,21,68,29,18,25","77,29,49,11,51,53","36,49,94,59,22,27","63,78,96,44,41,88",
+  "33,39,43,51,55,36","21,79,74,85,63,82","34,43,27,53,17,57","24,14,11,15,75,78","16,44,49,54,49,99",
+  "31,35,67,44,84,42","28,53,21,34,52,56","63,35,12,31,81,15","32,65,16,67,73,55","63,28,76,45,69,62",
+  "85,56,75,37,73,24","54,42,68,13,99,84","14,27,54,85,59,69","68,42,28,34,83,16","69,28,45,37,71,91"]
+operations(e,no13)
+###########################################################
+
+# 14
+ex+=1
+sum +=1
+table1(e,sum,ex,list)
+#
+
+# TODO ex. no. 15
+
+
+
